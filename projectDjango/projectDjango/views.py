827db95b873696e4c92fd6c5d948e433c2cb49be
Abhinav <abhinavkumarjha02@gmail.com>
2025-02-18 11:23:12 +0530

    some updated code

--- projectDjango/projectDjango/views.py
+++ projectDjango/projectDjango/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    # return HttpResponse("Hello, world. You are at my Project's home page")
-    return render(request, 'index.html')
+    return HttpResponse("Hello, world. You are at my Project's home page")
+    # return render(request, 'index.html')
 
 def about(request):
     return HttpResponse("Hello, world. You are at my Project's about page")
